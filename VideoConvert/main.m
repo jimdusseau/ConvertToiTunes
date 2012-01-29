4a3a2ae0980545fdb7589dc3f706642b09988f6a
Jim Dusseau <j.dusseau@techsmith.com>
2012-01-29 16:42:40 -0500

    Added some missing files

---
+++ VideoConvert/main.m
@@ -0,0 +1,41 @@
+//
+//  main.m
+//  VideoConvert
+//
+//  Created by Jim Dusseau on 1/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ConversionEngine.h"
+
+int main (int argc, const char * argv[])
+{
+   
+   @autoreleasepool
+   {
+      if(argc == 2)
+      {
+         NSString *path = [NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding];
+         NSURL *pathURL = [NSURL fileURLWithPath:path];
+         
+         NSString *tempPath = [NSTemporaryDirectory() stringByAppendingPathComponent:[path lastPathComponent]];
+         tempPath  = [[tempPath stringByDeletingPathExtension] stringByAppendingPathExtension:@"m4v"];
+         NSURL *resultURL = [NSURL fileURLWithPath:tempPath];
+         
+         ConversionEngine *engine = [[ConversionEngine alloc] init];
+         engine.videoURL = pathURL;
+         engine.resultVideoURL = resultURL;
+         
+         [engine go];
+      }
+      else
+      {
+         NSString *appName = [NSString stringWithCString:argv[0] encoding:NSUTF8StringEncoding];
+         NSLog(@"Expected 1 argument! %@ [path to video] ", appName);
+      }
+      
+   }
+   return 0;
+}
+
