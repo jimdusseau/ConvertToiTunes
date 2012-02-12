0aba0eb579bdf2eb40ca995fa46b57dcc24f0e9b
Jim Dusseau <j.dusseau@techsmith.com>
2012-02-12 11:17:10 -0500

    Cleaned up the main file a little to bail with an error code if the incorrect number of args is passed in

--- VideoConvert/main.m
+++ VideoConvert/main.m
@@ -11,27 +11,24 @@
 
 int main (int argc, const char * argv[])
 {
-   
    @autoreleasepool
    {
-      if(argc == 2)
-      {
-         NSString *logPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Library/Logs/VideoConvert.log"];
-         freopen([logPath fileSystemRepresentation], "a", stderr);
-         
-         NSString *path = [NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding];
-         NSURL *pathURL = [NSURL fileURLWithPath:path];
-         
-         ConversionEngine *engine = [[ConversionEngine alloc] init];
-         engine.videoURL = pathURL;
-         [engine doConversion];
-      }
-      else
+      if(argc != 2)
       {
          NSString *appName = [NSString stringWithCString:argv[0] encoding:NSUTF8StringEncoding];
          NSLog(@"Expected 1 argument! %@ [path to video] ", appName);
+         return 1;
       }
       
+      NSString *logPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Library/Logs/VideoConvert.log"];
+      freopen([logPath fileSystemRepresentation], "a", stderr);
+      
+      NSString *path = [NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding];
+      NSURL *pathURL = [NSURL fileURLWithPath:path];
+      
+      ConversionEngine *engine = [[ConversionEngine alloc] init];
+      engine.videoURL = pathURL;
+      [engine doConversion];
    }
    return 0;
 }
