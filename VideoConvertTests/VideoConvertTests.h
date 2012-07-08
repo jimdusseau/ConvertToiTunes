0795174de22ec65a7c9e6f6387e555b9cf578583
Jim Dusseau <j.dusseau@techsmith.com>
2012-07-08 11:12:25 -0400

    Added Unit Tests

---
+++ VideoConvertTests/VideoConvertTests.h
@@ -0,0 +1,13 @@
+//
+//  VideoConvertTests.h
+//  VideoConvertTests
+//
+//  Created by Jim Dusseau on 7/8/12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface VideoConvertTests : SenTestCase
+
+@end
---
+++ VideoConvertTests/VideoConvertTests.m
@@ -0,0 +1,32 @@
+//
+//  VideoConvertTests.m
+//  VideoConvertTests
+//
+//  Created by Jim Dusseau on 7/8/12.
+//
+//
+
+#import "VideoConvertTests.h"
+
+@implementation VideoConvertTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+   STAssertTrue(1 == 1, @"Testing harness should have been okay.");
+}
+
+@end
